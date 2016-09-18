6a7aaded9f8c4164c23ede460d30299c44e308f2
Neil Clayton <neil@shinywhitebox.com>
2016-09-18 12:23:27 +1200

    - Add project file

---
+++ InflectorKit/InflectorKit/InflectorKit/InflectorKit.h
@@ -0,0 +1,19 @@
+//
+//  InflectorKit.h
+//  InflectorKit
+//
+//  Created by Neil Clayton on 18/09/16.
+//  Copyright © 2016 shinywhitebox. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for InflectorKit.
+FOUNDATION_EXPORT double InflectorKitVersionNumber;
+
+//! Project version string for InflectorKit.
+FOUNDATION_EXPORT const unsigned char InflectorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InflectorKit/PublicHeader.h>
+
+
